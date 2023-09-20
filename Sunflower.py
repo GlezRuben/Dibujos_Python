8be46ef705c1984da9bb4b538a9c500c779758eb
Ruben González <99474897+GlezRuben@users.noreply.github.com>
2023-09-19 22:46:23 -0600

    Add files via upload

---
+++ Sunflower.py
@@ -0,0 +1,31 @@
+import math
+import turtle
+turtle.bgcolor("black")
+turtle.shape("turtle")
+turtle.speed(0)
+turtle.fillcolor("brown")
+phi = 137.508 * (math.pi / 180.0)
+for i in range (160 + 40):
+    r = 4 * math.sqrt(i)
+    theta = i * phi
+    x = r * math.cos(theta)
+    y = r * math.sin(theta)
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.setheading(i * 137.508)
+    turtle.pendown()
+    if i < 160:
+        turtle.stamp()
+    else:
+        turtle.fillcolor("yellow")
+        turtle.begin_fill()
+        turtle.right(20)
+        turtle.forward(70)
+        turtle.left(40)
+        turtle.forward(70)
+        turtle.left(140)
+        turtle.forward(70)
+        turtle.left(40)
+        turtle.forward(70)
+        turtle.end_fill()
+turtle.hideturtle()
