c98f531c854b18ae18ad9ebdb623b71698cda579
Ruben González <99474897+GlezRuben@users.noreply.github.com>
2024-02-06 18:51:52 -0600

    Add files via upload

---
+++ corazon_lineas.py
@@ -0,0 +1,30 @@
+import math
+import turtle
+
+t = turtle.Turtle()
+turtle.speed(2000)
+turtle.bgcolor("black")
+
+def corazon(k):
+    return 15*math.sin(k)**3
+
+def corazon1(k):
+    return 12*math.cos(k)-5*\
+           math.cos(2*k)-2*\
+           math.cos(3*k)-\
+           math.cos(4*k)
+
+for i in range(600):
+    t.goto(corazon(i)*18, corazon1(i)*18)
+    for j in range(5):
+        t.color("red")
+        
+t.color("gold")
+t.penup()
+t.goto(115,-40)
+t.pendown()
+t.write("I LOVE YOU", False,
+      "right", ("arial", 30, "bold"))
+
+t.hideturtle()
+turtle.done()
