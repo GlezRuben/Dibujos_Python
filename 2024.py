567ee7f69dfcbd940b4c568c7aa22c6e30861eda
Ruben González <99474897+GlezRuben@users.noreply.github.com>
2024-11-02 12:07:47 -0600

    Add files via upload

---
+++ 2024.py
@@ -0,0 +1,202 @@
+import turtle
+
+turtle.bgcolor("Black")
+t = turtle.Turtle()
+t.pensize(11)
+
+def go(x,y):
+    t.penup()
+    t.goto(x,y)
+    t.pendown()
+    
+t.pencolor("White")
+
+# 2
+go(-138.70,8.55)
+t.seth(22.4)
+t.circle(16.23,135.2)
+t.seth(180)
+t.forward(59.78)
+t.seth(59.23)
+t.circle(-121.62,21.58)
+t.seth(35.41)
+t.circle(102.86,41.45)
+t.seth(86.53)
+t.circle(52.42,182.79)
+t.seth(271)
+t.circle(14.97,174.51)
+t.seth(85.51)
+t.circle(-22.29,170.28)
+t.seth(254.67)
+t.circle(-65.20,41.13)
+t.seth(215.95)
+t.circle(164.06,17.82)
+t.seth(240.85)
+t.circle(86.83,29.88)
+t.seth(287.38)
+t.circle(17.60,53.35)
+t.seth(0)
+t.forward(82.31)
+
+# Youtube: https://www.youtube.com/@glez_rbn
+# TikTok: @glez_rbn
+# Instagram: @glez_rbn
+
+# 0
+go(-64.02,41.74)
+t.seth(51.68)
+t.circle(91.63,75.1)
+t.seth(135)
+t.circle(19.74,90)
+t.seth(233.28)
+t.circle(91.94,73.43)
+t.seth(310.38)
+t.circle(18.46,91.67)
+
+go(-15.15,95.05)
+t.seth(80.51)
+t.circle(30.09,18.99)
+
+go(-42.13,173.52)
+t.seth(137.67)
+t.circle(51.81,90)
+
+go(-136.72,79.24)
+t.seth(267.07)
+t.circle(48.16,29.34)
+
+go(-136.72,79.24)
+t.seth(267.07)
+t.circle(48.16,29.34)
+
+go(-110.05,21.24)
+t.seth(312.18)
+t.circle(44.57,90)
+
+# Youtube: https://www.youtube.com/@glez_rbn
+# TikTok: @glez_rbn
+# Instagram: @glez_rbn
+
+# 2
+go(65.26,8.55)
+t.seth(22.4)
+t.circle(16.23,135.2)
+t.seth(180)
+t.forward(59.78)
+t.seth(59.23)
+t.circle(-121.62,21.58)
+t.seth(35.41)
+t.circle(102.86,41.45)
+t.seth(86.53)
+t.circle(52.42,182.79)
+t.seth(271)
+t.circle(14.97,174.51)
+t.seth(85.51)
+t.circle(-22.29,170.28)
+t.seth(254.67)
+t.circle(-65.20,41.13)
+t.seth(215.95)
+t.circle(164.06,17.82)
+t.seth(240.85)
+t.circle(86.83,29.88)
+t.seth(287.38)
+t.circle(17.60,53.35)
+t.seth(0)
+t.forward(82.31)
+
+# Caída
+go(89.17,-12.35)
+t.seth(270)
+t.forward(12)
+
+go(146.23,38.57)
+t.seth(270)
+t.forward(38.57)
+
+go(146.23,-21.42)
+t.seth(270)
+t.forward(15.80)
+
+go(182.81,-71.75)
+t.seth(270)
+t.forward(26.92)
+
+go(214.99,-28.44)
+t.seth(270)
+t.forward(39.50)
+
+t.pencolor("Red")
+
+# Youtube: https://www.youtube.com/@glez_rbn
+# TikTok: @glez_rbn
+# Instagram: @glez_rbn
+
+# 3
+go(72.46,-97.39)
+t.seth(224.11)
+t.circle(60.52,38.53)
+t.seth(267.66)
+t.circle(15.33,180)
+t.seth(74.68)
+t.circle(-33.86,75.33)
+t.seth(340.67)
+t.circle(-23,90)
+t.seth(221.47)
+t.forward(24.32)
+t.seth(244.23)
+t.circle(15.49,145.71)
+t.seth(41.47)
+t.forward(24.20)
+t.seth(29.9)
+t.circle(-23.57,125.45)
+t.seth(261.42)
+t.circle(-44.81,69.71)
+t.seth(191.71)
+t.circle(15.28,192.62)
+t.seth(15.88)
+t.circle(81.91,51.15)
+t.seth(68.96)
+t.circle(54.95,110.38)
+t.seth(97.28)
+t.circle(53.63,130.99)
+
+t.pencolor("Gold")
+
+# Youtube: https://www.youtube.com/@glez_rbn
+# TikTok: @glez_rbn
+# Instagram: @glez_rbn
+
+# 4
+go(113.70,96.84)
+t.seth(0)
+t.forward(53)
+t.seth(270)
+t.forward(30.07)
+t.seth(281.66)
+t.circle(16.31,156.69)
+t.seth(90)
+t.forward(30.67)
+t.seth(0)
+t.forward(14.66)
+t.seth(0)
+t.circle(15.03,180)
+t.seth(180)
+t.forward(14.66)
+t.seth(90)
+t.forward(94.50)
+t.seth(94.92)
+t.circle(14.71,130.75)
+t.seth(235.77)
+t.forward(132.29)
+t.seth(234.33)
+t.circle(15.74,132.5)
+
+go(143.77,129.54)
+t.seth(0)
+t.forward(22.93)
+t.seth(90)
+t.forward(32.70)
+t.seth(234.96)
+t.forward(39.94)
+
+t.hideturtle()
